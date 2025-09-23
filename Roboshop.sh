0c5de78087ce57a31dbb559435c79f0be8b17ad1
naginenihari <naginenihari@gmail.com>
2025-09-23 18:34:46 +0530

    roboshop code updated

--- Roboshop.sh
+++ Roboshop.sh
@@ -7,7 +7,7 @@ SUBNET_ID="subnet-0f6a0c42281790747"
 for instance in $@
 do
  INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SG_ID --subnet-id $SUBNET_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
- if [$instance !- "Frontend"]; then
+ if [ $instance !- "frontend" ]; then
     IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
  else
     IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text) 
