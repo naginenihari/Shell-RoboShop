f50c23d7655914cedac4c81fa3678fdbf6f2b215
naginenihari <naginenihari@gmail.com>
2025-09-23 18:11:16 +0530

    roboshop project ec2 server creation script updated

---
+++ Roboshop.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+AMI_ID="ami-09c813fb71547fc4f"
+SG_ID="sg-011e888af9e03fb6e"
+SUBNET_ID="subnet-0f6a0c42281790747"
+
+for instance in $@
+do
+ INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SG_ID --subnet-id $SUBNET_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]")
+ if [$instance !- "Frontend"]; then
+    IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+ else
+    IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text) 
+ fi
+    echo "$instance: $IP"
+done
