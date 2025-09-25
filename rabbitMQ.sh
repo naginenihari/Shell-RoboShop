0409de33aa8078acdd8b03b4fb07ae81b15be162
naginenihari <naginenihari@gmail.com>
2025-09-25 21:10:46 +0530

    rabbitmq.sh file updated

---
+++ rabbitMQ.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[37m"
+
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$(echo $0 |cut -d '.' -f1)
+SCRIPT_DIR=$PWD
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+START_TIME=$(date +%s)
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executed at:$(date)" |tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: please run the script with root privelege" |tee -a $LOG_FILE
+    exit 1  #failure mean other then Zero
+fi
+
+VALIDATE() {
+if [ $1 -ne 0 ]; then
+    echo -e " $2 is $R FAILURE $N" |tee -a $LOG_FILE
+    exit 1
+else 
+    echo -e " $2 is $G SUCCESS $N" |tee -a $LOG_FILE
+fi
+}
+
+cp $SCRIPT_DIR/rabbitmq.repo  /etc/yum.repos.d/rabbitmq.repo &>>$LOG_FILE
+VALIDATE $? "Adding RabbitMQ repo"
+dnf install rabbitmq-server -y &>>$LOG_FILE
+VALIDATE $? "Installing RabbitMQ Server"
+systemctl enable rabbitmq-server &>>$LOG_FILE
+VALIDATE $? "Enabling RabbitMQ Server"
+systemctl start rabbitmq-server &>>$LOG_FILE
+VALIDATE $? "Starting RabbitMQ Server"
+
+rabbitmqctl add_user roboshop roboshop123
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+VALIDATE $? "Setting up permissions"
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+echo -e "Script executed in: $Y $TOTAL_TIME Seconds $N"
