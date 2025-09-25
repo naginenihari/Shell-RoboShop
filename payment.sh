4b9d94683c0bb806d596731aceb42b87c2685677
naginenihari <naginenihari@gmail.com>
2025-09-25 22:13:46 +0530

    payments.sh file updated

---
+++ payment.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[37m"
+
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$(echo $0 |cut -d '.' -f1)
+MONGODB_HOST=mongodb.naginenihariaws.store
+SCRIPT_DIR=$PWD
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+START_TIME=$(date +%s)
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executed at:$(date)" |tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: please run the script with root privelege" |tee -a $LOG_FILE
+    exit 1  #failure mean other then Zero
+fi
+
+VALIDATE() {
+if [ $1 -ne 0 ]; then
+    echo -e " $2 is $R FAILURE $N" |tee -a $LOG_FILE
+    exit 1
+else 
+    echo -e " $2 is $G SUCCESS $N" |tee -a $LOG_FILE
+fi
+}
+
+dnf install python3 gcc python3-devel -y &>>$LOG_FILE
+VALIDATE $? "Installed NodeJS"
+
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+VALIDATE $? "Creating system user"
+
+mkdir /app &>>$LOG_FILE
+VALIDATE $? "Creating app directory"
+
+
+curl -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment-v3.zip &>>$LOG_FILE
+VALIDATE $? "Downloading payment application source code"
+
+cd /app 
+VALIDATE $? "Changing to app directory"
+unzip /tmp/payment.zip &>>$LOG_FILE
+VALIDATE $? "unzip the payment code"
+pip3 install -r requirements.txt
+
+cp $SCRIPT_DIR/payment.service /etc/systemd/system/payment.service &>>$LOG_FILE
+VALIDATE $? "copy systemctl service"
+systemctl daemon-reload
+systemctl enable payment &>>$LOG_FILE
+VALIDATE $? "Enable payment Service"
+systemctl restart payment &>>$LOG_FILE
+VALIDATE $? "Restarted payment Service"
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+echo -e "Script executed in: $Y $TOTAL_TIME Seconds $N"
