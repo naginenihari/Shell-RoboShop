77e71e64df89ce084e424d944763a65d8303a9e7
naginenihari <naginenihari@gmail.com>
2025-09-25 10:39:22 +0530

    mongosh file updated

---
+++ Roboshop.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+AMI_ID="ami-09c813fb71547fc4f"
+SG_ID="sg-011e888af9e03fb6e"
+SUBNET_ID="subnet-0f6a0c42281790747"
+HOSTED_ZONE="Z05202297D7QR4S9L2JB"
+DOMAIN_NAME="naginenihariaws.store"
+
+for instance in $@
+do
+ INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SG_ID --subnet-id $SUBNET_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
+ if [ $instance != "frontend" ]; then
+    IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+    RECORD_NAME="$instance.$DOMAIN_NAME"
+ else
+     IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text) 
+     RECORD_NAME="$DOMAIN_NAME"
+ fi
+    echo "$instance: $IP"
+
+aws route53 change-resource-record-sets --hosted-zone-id $HOSTED_ZONE --change-batch '
+  {
+    "Comment": "creating record"
+    ,"Changes": [{
+      "Action"              : "CREATE"
+      ,"ResourceRecordSet"  : {
+        "Name"              : "'$RECORD_NAME'"
+        ,"Type"             : "A"
+        ,"TTL"              : 1
+        ,"ResourceRecords"  : [{
+            "Value"         : "'$IP'"
+        }]
+      }
+    }]
+  }
+  '
+  done
