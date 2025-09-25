3f06f6240efddbe0459ea28855c9762314a90d7f
naginenihari <naginenihari@gmail.com>
2025-09-25 21:14:18 +0530

    rabbitmq.sh file updated

--- rabbitMQ.sh
+++ rabbitMQ.sh
@@ -38,8 +38,8 @@ VALIDATE $? "Enabling RabbitMQ Server"
 systemctl start rabbitmq-server &>>$LOG_FILE
 VALIDATE $? "Starting RabbitMQ Server"
 
-rabbitmqctl add_user roboshop roboshop123
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+rabbitmqctl add_user roboshop roboshop123   &>>$LOG_FILE
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"  &>>$LOG_FILE
 VALIDATE $? "Setting up permissions"
 
 END_TIME=$(date +%s)
