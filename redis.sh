4437d4567ad6cd77ff9456ffe2883ff1b447ec50
naginenihari <naginenihari@gmail.com>
2025-09-25 20:07:36 +0530

    redis.sh file updated

---
+++ redis.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[37m"
+
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$(echo $0 |cut -d '.' -f1)
+MONGODB_HOST=mongodb.naginenihariaws.store
+SCRIPT_DIR=$PWD
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+START_TIME=(date +%s)
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executed at:$(date)" |tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: please run the script with root privelege" |tee -a $LOG_FILE
+    exit 1  #failure mean other then Zero
+fi
+
+VALIDATE() {
+if [ $1 -ne 0 ]; then
+    echo -e " $2 is $R FAILURE $N" |tee -a $LOG_FILE
+    exit 1
+else 
+    echo -e " $2 is $G SUCCESS $N" |tee -a $LOG_FILE
+fi
+}
+
+dnf module disable redis -y &>>$LOG_FILE
+VALIDATE $? "Disabling Deafult redis"
+dnf module enable redis:7 -y &>>$LOG_FILE
+VALIDATE $? "Enabling redis 7"
+dnf install redis -y &>>$LOG_FILE
+VALIDATE $? "Installing redis "
+
+sed -i -e 's/127.0.0.1/0.0.0.0' -e '/protected-mode/ c protected-mode no' &>>$LOG_FILE
+VALIDATE $? "Allowing Remote connections to Redis"
+
+systemctl enable redis &>>$LOG_FILE
+VALIDATE $? "Enabling Redis"
+systemctl start redis &>>$LOG_FILE
+VALIDATE $? "Starting Redis"
