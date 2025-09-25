17fc5093fc55066a22a69ba5defee4b053e6b29a
naginenihari <naginenihari@gmail.com>
2025-09-25 20:30:05 +0530

    redis.sh file updated

--- redis.sh
+++ redis.sh
@@ -37,7 +37,7 @@ VALIDATE $? "Enabling redis 7"
 dnf install redis -y &>>$LOG_FILE
 VALIDATE $? "Installing redis "
 
-sed -i -e 's/127.0.0.1/0.0.0.0' -e '/protected-mode/ c protected-mode no' &>>$LOG_FILE
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf &>>$LOG_FILE
 VALIDATE $? "Allowing Remote connections to Redis"
 
 systemctl enable redis &>>$LOG_FILE
