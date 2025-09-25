c9683f02a44fc79b43e6838c6a3a1ed844cdf741
naginenihari <naginenihari@gmail.com>
2025-09-25 16:17:19 +0530

    catalogue.sh file updated

--- catalogue.sh
+++ catalogue.sh
@@ -43,7 +43,7 @@ if [ $? -ne 0 ]; then
  useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
  VALIDATE $? "Creating system user"
 else
- echo -e"user is already exist ..$Y SKIPPING $N"
+ echo -e "$Y user is already exist ..$Y SKIPPING $N"
 fi
 mkdir -p /app &>>$LOG_FILE
 VALIDATE $? "Creating app directory"
