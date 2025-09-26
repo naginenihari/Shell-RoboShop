d351176ba6f0d81931e367272b5babdff2678866
naginenihari <naginenihari@gmail.com>
2025-09-26 10:20:19 +0530

    rabbitmq.sh file updated

--- rabbitMQ.sh
+++ rabbitMQ.sh
@@ -29,10 +29,12 @@ else
 fi
 }
 
+##RabbitMQ Installation
 cp $SCRIPT_DIR/rabbitmq.repo  /etc/yum.repos.d/rabbitmq.repo &>>$LOG_FILE
 VALIDATE $? "Adding RabbitMQ repo"
 dnf install rabbitmq-server -y &>>$LOG_FILE
 VALIDATE $? "Installing RabbitMQ Server"
+
 systemctl enable rabbitmq-server &>>$LOG_FILE
 VALIDATE $? "Enabling RabbitMQ Server"
 systemctl start rabbitmq-server &>>$LOG_FILE
