00f6709fd7f125f0a123c821bc5cbbd4a9bdbf0f
naginenihari <naginenihari@gmail.com>
2025-09-26 11:57:03 +0530

    payments.sh file updated

--- payment.sh
+++ payment.sh
@@ -4,34 +4,34 @@ USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
-N="\e[37m"
+N="\e[0m"
 
 LOGS_FOLDER="/var/log/shell-roboshop"
-SCRIPT_NAME=$(echo $0 |cut -d '.' -f1)
-MONGODB_HOST=mongodb.naginenihariaws.store
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 SCRIPT_DIR=$PWD
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-START_TIME=$(date +%s)
+MONGODB_HOST=mongodb.daws86s.fun
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
+MYSQL_HOST=mysql.daws86s.fun
+END_TIME=$(date +%s)
 
 mkdir -p $LOGS_FOLDER
-echo "Script started executed at:$(date)" |tee -a $LOG_FILE
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
 
 if [ $USERID -ne 0 ]; then
-    echo "ERROR:: please run the script with root privelege" |tee -a $LOG_FILE
-    exit 1  #failure mean other then Zero
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1 # failure is other than 0
 fi
 
-VALIDATE() {
-if [ $1 -ne 0 ]; then
-    echo -e " $2 is $R FAILURE $N" |tee -a $LOG_FILE
-    exit 1
-else 
-    echo -e " $2 is $G SUCCESS $N" |tee -a $LOG_FILE
-fi
+VALIDATE(){ # functions receive inputs through args just like shell script args
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOG_FILE
+    fi
 }
-##Python Installation ##
+
 dnf install python3 gcc python3-devel -y &>>$LOG_FILE
-VALIDATE $? "Installing  python3"
 
 id roboshop &>>$LOG_FILE
 if [ $? -ne 0 ]; then
@@ -41,28 +41,28 @@ else
     echo -e "User already exist ... $Y SKIPPING $N"
 fi
 
-mkdir /app &>>$LOG_FILE
+mkdir -p /app
 VALIDATE $? "Creating app directory"
 
 curl -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment-v3.zip &>>$LOG_FILE
-VALIDATE $? "Downloading payment application source code"
+VALIDATE $? "Downloading payment application"
 
 cd /app 
 VALIDATE $? "Changing to app directory"
 
+rm -rf /app/*
+VALIDATE $? "Removing existing code"
+
 unzip /tmp/payment.zip &>>$LOG_FILE
-VALIDATE $? "unzip the payment code"
+VALIDATE $? "unzip payment"
 
-pip3 install -r requirements.txt
-VALIDATE $? "Downloading Dependences"
+pip3 install -r requirements.txt &>>$LOG_FILE
 
-cp $SCRIPT_DIR/payment.service /etc/systemd/system/payment.service &>>$LOG_FILE
-VALIDATE $? "copy systemctl service"
+cp $SCRIPT_DIR/payment.service /etc/systemd/system/payment.service
 systemctl daemon-reload
-systemctl enable payment &>>$LOG_FILE
-VALIDATE $? "Enable payment Service"
-systemctl restart payment &>>$LOG_FILE
-VALIDATE $? "Restarted payment Service"
+systemctl enable payment  &>>$LOG_FILE
+
+systemctl restart payment
 
 END_TIME=$(date +%s)
 TOTAL_TIME=$(( $END_TIME - $START_TIME ))
