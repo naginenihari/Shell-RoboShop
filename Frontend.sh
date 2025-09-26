7fe131832075f74ba2fb5c5dbe20b46c34caa072
naginenihari <naginenihari@gmail.com>
2025-09-26 10:36:20 +0530

    frontend.sh file updated

--- Frontend.sh
+++ Frontend.sh
@@ -29,7 +29,7 @@ else
     echo -e " $2 is $G SUCCESS $N" |tee -a $LOG_FILE
 fi
 }
-
+##Installing Nginx ##
 dnf module disable nginx -y &>>$LOG_FILE
 VALIDATE $? "Disabling Nginx"
 
@@ -45,19 +45,17 @@ VALIDATE $? "Enabled Nginx"
 rm -rf /usr/share/nginx/html/* 
 
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>$LOG_FILE
-VALIDATE $? "Downloading user application source code"
+VALIDATE $? "Downloading user application sourcecode"
 
 cd /usr/share/nginx/html
 VALIDATE $? "Changing to app directory"
 unzip /tmp/frontend.zip &>>$LOG_FILE
 VALIDATE $? "unzip the user code"
 
-
+rm -rf /etc/nginx/nginx.conf
+VALIDATE $? "Removing default nginx config file"
 cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf &>>$LOG_FILE
-VALIDATE $? "copy systemctl service"
-systemctl daemon-reload
-systemctl enable nginx &>>$LOG_FILE
-VALIDATE $? "Enable nginx Service"
+VALIDATE $? "copying nginx config file"
 systemctl restart nginx &>>$LOG_FILE
 VALIDATE $? "Restarted nginx Service"
 
